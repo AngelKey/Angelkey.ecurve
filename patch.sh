29a0eb03af3c3853933ede8f9a2ba02d962b5e1e
Maxwell Krohn <themax@gmail.com>
2014-06-13 14:11:18 -0400

    patch

--- patch.sh
+++ patch.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# Run this on fresh checkout for our patches...
+
 FIND="require('bigi')"
 REPLACE="require('bn').BigInteger"
 regex="s/${FIND}/${REPLACE}/"
 
-sed -i.bak ${regex} lib/*.js
-rm lib/*.js.bak
+find test lib -type f -regex '.*\.js' -exec sed -i.bak ${regex} {} \;
+find test lib -type f -regex '.*\.js.bak' -exec rm {} \;
